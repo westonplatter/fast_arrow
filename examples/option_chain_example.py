de1e8cea910df6babcfef4f9f73dcca7566b082f
Weston Platter <westonplatter@gmail.com>
2018-07-28 22:43:39 -0600

    add concrete example fetching all TLT options for next 3 expiration dates.

---
+++ examples/option_chain_example.py
@@ -0,0 +1,47 @@
+import configparser
+import time
+
+import fast_arrow as fa
+from fast_arrow.resources.auth import Auth
+from fast_arrow.resources.stock import Stock
+from fast_arrow.resources.option_chain import OptionChain
+from fast_arrow.resources.option import Option
+
+#
+# get the authentication configs
+#
+config_file = "config.debug.ini"
+config = configparser.ConfigParser()
+config.read(config_file)
+username = config['account']['username']
+password = config['account']['password']
+
+#
+# login and get the bearer token
+#
+token = Auth.login(username, password)
+bearer = Auth.get_oauth_token(token)
+
+#
+# fetch the stock info for TLT
+#
+symbol = "TLT"
+stock = Stock.fetch(bearer, symbol)
+
+#
+# get the TLT option chain info
+#
+stock_id = stock["id"]
+option_chain = OptionChain.fetch(bearer, stock_id)
+option_chain_id = option_chain["id"]
+expiration_dates = option_chain['expiration_dates']
+
+#
+# reduce the number of expiration dates we're interested in
+#
+next_3_expiration_dates = expiration_dates[0:3]
+
+#
+# get all options on the TLT option chain
+#
+tlt_options = Option.all(bearer, option_chain_id, next_3_expiration_dates)
--- fast_arrow/resources/option.py
+++ fast_arrow/resources/option.py
@@ -22,3 +22,23 @@ class Option(object):
         params = {"instruments": id_url}
         data = get(url, bearer=bearer, params=params)
         return data["results"][0]
+
+
+    @classmethod
+    def all(cls, bearer, chain_id, expiration_dates):
+        """
+        fetch all options in a options chain for given expiration dates
+        """
+        assert(type(expiration_dates) is list)
+
+        url = "https://api.robinhood.com/options/instruments/"
+        params = {
+            "chain_id": chain_id,
+            "expiration_dates": ",".join(expiration_dates)
+        }
+        data = get(url, bearer=bearer, params=params)
+        results = data['results']
+        while data['next']:
+            data = get(data['next'], bearer=bearer)
+            results.extend(data['results'])
+        return results
