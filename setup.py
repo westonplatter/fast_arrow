29da6743add3fbf07f2309ba119a2af13b0d196b
Weston Platter <westonplatter@gmail.com>
2018-07-31 23:28:38 -0600

    version bump. 0.0.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fh:
 
 
 setup(name='fast_arrow',
-    version='0.0.1',
+    version='0.0.2',
     description='API client for Robinhood',
     long_description=long_description,
     long_description_content_type="text/markdown",
