da4fc2618fe5079f083f7ad1bc12e4906fb80239
Weston Platter <westonplatter@gmail.com>
2018-08-25 13:06:39 -0600

    fix ci.

--- fast_arrow/resources/tag.py
+++ fast_arrow/resources/tag.py
@@ -1,12 +1,9 @@
-from fast_arrow.api_requestor import get
-
-
 class Tag(object):
 
     @classmethod
-    def fetch_instruments_by_tag(cls, bearer, tag):
+    def fetch_instruments_by_tag(cls, client, tag):
         base_url = "https://api.robinhood.com/midlands/tags/tag/"
         url = "{}{}/".format(base_url, tag.lower())
-        res = get(url, bearer=bearer)
+        res = client.get(url)
         result = (res["instruments"] if "instruments" in res else [])
         return result
