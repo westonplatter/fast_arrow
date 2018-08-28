d9ec95b4923f4fd520552aac12224f8391c6c9d0
Weston Platter <westonplatter@gmail.com>
2018-08-27 19:33:50 -0600

    fix #22. ensure option chain is symbol specific.

--- fast_arrow/resources/option_chain.py
+++ fast_arrow/resources/option_chain.py
@@ -1,15 +1,18 @@
 class OptionChain(object):
 
     @classmethod
-    def fetch(cls, client, id):
+    def fetch(cls, client, _id, symbol):
         """
         fetch option chain for instrument
         """
         url = "https://api.robinhood.com/options/chains/"
         params = {
-            "equity_instrument_ids": [id],
+            "equity_instrument_ids": _id,
             "state": "active",
             "tradability": "tradable"
         }
         data = client.get(url, params=params)
-        return data['results'][0]
+        def filter_func(x):
+            return x["symbol"] == symbol
+        results = list(filter(filter_func, data["results"]))
+        return results[0]
