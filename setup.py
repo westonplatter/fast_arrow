a14a9cf080e3a5e40808acdbabe3cba642d0a1cc
Weston Platter <westonplatter@gmail.com>
2018-09-03 15:51:00 -0400

    verion bump. added #26, portfolio historical data.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ test_deps = [
     'vcrpy']
 
 setup(name='fast_arrow',
-    version='0.2.1',
+    version='0.2.2',
     description='API client for Robinhood',
     long_description=long_description,
     long_description_content_type="text/markdown",
