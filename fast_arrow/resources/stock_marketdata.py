2a08fab980bf2c53dafa2784265bfeb1c510cd04
westonplatter <westonplatter@gmail.com>
2019-04-13 14:57:18 -0600

    feature #17. fetch historical stock data by symbol or symbols.

--- fast_arrow/resources/stock_marketdata.py
+++ fast_arrow/resources/stock_marketdata.py
@@ -1,4 +1,7 @@
 from fast_arrow.util import chunked_list
+import deprecation
+from fast_arrow.version import VERSION
+
 
 
 class StockMarketdata(object):
@@ -50,7 +53,19 @@ class StockMarketdata(object):
         return results
 
     @classmethod
+    @deprecation.deprecated(
+        deprecated_in="0.3.1", removed_in="0.4",current_version=VERSION,
+        details="Use 'historical_quote_by_symbol' instead")
     def historical(cls, client, symbol, span="year", bounds="regular"):
+        return cls.historical_quote_by_symbol(client, symbol, span, bounds)
+
+    @classmethod
+    def historical_quote_by_symbol(cls, client, symbols, span="year", bounds="regular"):
+        datas = cls.historical_quote_by_symbols(client, [symbol],span, bounds)
+        return datas[0]
+
+    @classmethod
+    def historical_quote_by_symbols(cls, client, symbols, span="year", bounds="regular"):
         possible_intervals = {
             "day": "5minute",
             "week": "10minute",
@@ -60,12 +75,13 @@ class StockMarketdata(object):
         interval = possible_intervals[span]
         assert bounds in ["regular", "trading"]
 
-        request_url = "https://api.robinhood.com/marketdata/historicals/{}/".format(symbol)
-        params = {
-            "span":     span,
-            "interval": interval,
-            "bounds":   bounds,
-            "symbol":   symbol
-        }
-        data = client.get(request_url, params=params)
-        return data
+        request_url = "https://api.robinhood.com/marketdata/historicals/"
+
+        results = []
+        for _symbols in chunked_list(symbols, 25):
+            params = { "span": span, "interval": interval, "bounds": bounds,
+                       "symbols": ",".join(_symbols) }
+            data = client.get(request_url, params=params)
+            if data and data["results"]:
+                results.extend(data["results"])
+        return results
