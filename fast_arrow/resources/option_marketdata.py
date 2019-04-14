2c1c4673edcd3e4773a864e56646c3ac3ff8a2b0
westonplatter <westonplatter@gmail.com>
2019-04-14 11:59:39 -0600

    formatting.

--- fast_arrow/resources/option_marketdata.py
+++ fast_arrow/resources/option_marketdata.py
@@ -7,18 +7,15 @@ class OptionMarketdata(object):
     def quote_by_instrument_id(cls, client, _id):
         return cls.quotes_by_instrument_ids(client, [_id])[0]
 
-
     @classmethod
     def quotes_by_instrument_ids(cls, client, ids):
         base_url = "https://api.robinhood.com/options/instruments/"
         id_urls = ["{}{}/".format(base_url, _id) for _id in ids]
         return cls.quotes_by_instrument_urls(client, id_urls)
 
-
     @classmethod
     def quote_by_instrument_url(cls, client, url):
         return cls.quotes_by_instrument_urls(client, [url])[0]
-
 
     @classmethod
     def quotes_by_instrument_urls(cls, client, urls):
@@ -30,16 +27,14 @@ class OptionMarketdata(object):
             if data and "results" in data:
                 partial_results = data["results"]
                 while ("next" in data and data["next"]):
-                    data = client.get(data["next"], bearer=bearer)
+                    data = client.get(data["next"])
                     partial_results.extend(data["results"])
                 results.extend(partial_results)
         return results
 
-
     @classmethod
     def historical_quote_by_id(cls, client, _id, span="year"):
         return cls.historical_quotes_by_ids(client, [_id], span)[0]
-
 
     @classmethod
     def historical_quotes_by_ids(cls, client, ids, span="year"):
@@ -47,11 +42,9 @@ class OptionMarketdata(object):
         urls = ["{}{}/".format(base_url, _id) for _id in ids]
         return cls.historical_quotes_by_urls(client, urls)
 
-
     @classmethod
     def historical_quote_by_url(cls, client, url, span="year"):
         return cls.historical_quotes_by_urls(client, [url], span)[0]
-
 
     @classmethod
     def historical_quotes_by_urls(cls, client, urls, span="year"):
@@ -59,13 +52,15 @@ class OptionMarketdata(object):
             "day": "5minute",
             "week": "10minute",
             "year": "day",
-            "5year": "week" }
+            "5year": "week"}
         assert span in possible_intervals.keys()
         interval = possible_intervals[span]
         results = []
-        request_url = "https://api.robinhood.com/marketdata/options/historicals/"
+        request_url = \
+            "https://api.robinhood.com/marketdata/options/historicals/"
         for _urls in chunked_list(urls, 5):
-            params = { "span": span, "interval": interval, "instruments": ",".join(_urls) }
+            params = {"span": span, "interval": interval,
+                      "instruments": ",".join(_urls)}
             data = client.get(request_url, params=params)
             if data and data["results"]:
                 results.extend(data["results"])
